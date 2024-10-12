39f43690cd2972b46c37352dd792ca4c1f240f2e
carlosd <carlosd@clubegazetadopovo.com.br>
2024-10-12 11:17:52 -0300

    feat: change userUpdateController.spec.js to .ts and adjust types

--- src/controllers/tests/userUpdateController.spec.ts
+++ src/controllers/tests/userUpdateController.spec.ts
@@ -1,30 +1,33 @@
-import { userUpdateUseCase } from "../../use-cases/userUpdateUseCase.js"
+import { Request, Response } from "express"
 import { hashPassword } from "../../utils/hashPassword"
 import userUpdateController from "../userUpdateController"
+import { userUpdateUseCase } from "../../use-cases/userUpdateUseCase"
 
-jest.mock('../../use-cases/userUpdateUseCase.js')
+jest.mock('../../use-cases/userUpdateUseCase.ts')
 
 describe('userUpdateController', () => {
   test('should user update', async () => {
     const req = {
       body: {
+        id: '1',
         name: 'John Doe',
         document: '000.000.000-00',
         phone: '(00) 00000-0000',
         password: hashPassword('123456')
       }
-    }
+    } as Request
 
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn().mockReturnThis(),
       message: jest.fn()
-    }
+    } as unknown as Response
 
     await userUpdateController(req, res)
 
     expect(userUpdateUseCase).toHaveBeenCalledTimes(1)
     expect(userUpdateUseCase).toHaveBeenCalledWith({
+      id: '1',
       name: 'John Doe',
       document: '000.000.000-00',
       phone: '(00) 00000-0000',
